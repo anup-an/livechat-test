4b9ada981787c661ad702d61b862c50070d42f9c
anup-an <friek_an@yahoo.com>
2021-01-09 10:51:36 +0200

    Project started

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <form>
+          <label htmlFor="url"> 
+            <div>URL</div> 
+            <div><input type="text" id="url" name="url" placeholder="URL...." /></div>
+          </label>
+
+          <label htmlFor="image" className="image"> 
+            <div>IMAGE</div> 
+            <div><input type="text" id="image" name="image" placeholder="Image URL...." /></div>
+          </label>
+          <label htmlFor="image" className="title"> 
+            <div>Title</div> 
+            <div><input type="text" id="title" name="title" placeholder="Title...." /></div>
+          </label>
+          <label htmlFor="image" className="subtitle"> 
+            <div>Subtitle</div> 
+            <div><input type="text" id="subtitle" name="subtitle" placeholder="Subtitle...." /></div>
+          </label>
+          <button type="submit">Send</button>
+        </form>
+
+      </div>
+    );
+  }
 }
 
 export default App;
